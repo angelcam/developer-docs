15ecce8985a2b8cbf5424ca79046e43e4a5386d8
Rudolf K <westfood@gmail.com>
2018-04-24 18:36:49 +0200

    Let's encrypt FTW.

--- ci/deploy/templates/run.sh
+++ ci/deploy/templates/run.sh
@@ -8,7 +8,8 @@ docker stop developer-docs >/dev/null 2>&1 || /bin/true
 docker rm -f developer-docs >/dev/null 2>&1 || /bin/true
 docker run --restart=always -d \
     -e VIRTUAL_HOST={{ virtual_host }} \
-    -e CERT_NAME={{ cert_name }} \
+    -e LETSENCRYPT_HOST={{ virtual_host }} \
+    -e LETSENCRYPT_EMAIL=devops@angelcam.com \
     --name developer-docs \
     --memory="500m" \
     angelcam/developer-docs:{{ tag }}
